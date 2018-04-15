260c0ef814b79576f43a91d1e8be83770daaf5b9
andixlm <andixev@gmail.com>
2018-04-15 18:48:17 +0300

    app: Import some logic

--- SmartHomeServer/MainWindow.xaml.cs
+++ SmartHomeServer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +16,58 @@ using System.Windows.Shapes;
 
 namespace SmartHomeServer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly int BUFFER_SIZE = 1024;
+
+        private static readonly string IPADDRESS_LOG_LABEL = "IP Address: ";
+
+        private static readonly string PORT_LOG_LABEL = "Port: ";
+        private static readonly int MINIMAL_PORT_VALUE = 1024;
+        private static readonly int MAXIMAL_PORT_VALUE = 49151;
+
+        private static readonly string CONNECTION_LOG_LABEL = "Connection: ";
+        private static readonly string CONNECTION_UP = "up";
+        private static readonly string CONNECTION_WAIT = "wait";
+        private static readonly string CONNECTION_DOWN = "down";
+        private static readonly string CONNECTION_ERR = "err";
+
+        private static readonly string UPDATE_INTERVAL_LOG_LABEL = "Update interval: ";
+
+        private static readonly string NETWORK_LOG_LABEL = "Network: ";
+
+        private static readonly string NETWORK_DEVICE_ARG = "Device: ";
+        private static readonly string NETWORK_TEMPERATURE_ARG = "Temparatute: ";
+        private static readonly string NETWORK_UPDATE_INTERVAL_ARG = "Update interval: ";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Init();
+            Configure();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void Configure()
+        {
+
+        }
+
+        private void Send(TcpClient socket, byte[] bytes)
+        {
+            NetworkStream stream = socket.GetStream();
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Flush();
+        }
+
+        private void ProcessData(string data)
+        {
+
         }
     }
 }
