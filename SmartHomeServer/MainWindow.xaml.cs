d5141829fd7cb98832014dcf3d2025bd2f9b5b17
andixlm <andixev@gmail.com>
2018-05-07 01:06:45 +0300

    app: Check data for emptiness

--- SmartHomeServer/MainWindow.xaml.cs
+++ SmartHomeServer/MainWindow.xaml.cs
@@ -349,7 +349,7 @@ namespace SmartHomeServer
             Receive(ref socket, ref bytes);
 
             string data = Encoding.Unicode.GetString(bytes);
-            if (string.IsNullOrEmpty(data) && data.Equals(""))
+            if (string.IsNullOrEmpty(data) || data.Equals(""))
             {
                 Log(NETWORK_LOG_LABEL + "Empty data received." + "\n");
                 return;
